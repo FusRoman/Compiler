67fba01c76be26182acb6d04bb2da5827684cddb
FusRoman <roman.lemontagner@gmail.com>
2019-11-09 20:52:32 +0100

    ajout de commentaire

--- utils/CLLTree.ml
+++ utils/CLLTree.ml
@@ -51,6 +51,9 @@ let for_to_while init cond it block =
   Cycle.append result (While(cond, block'))
 
 let rec make_tag e = 
+  (* cette fonction prend en parametre le nom d'une procedure et creer son etiquette 
+  de retour, ne devrait normalement pas poser de problème de duplication de tag vis a vis
+  du programmeur vu qu'il n'est pas possible de déclarer d'étiquette en CLL *)
   match e with
   |Id s -> TagDeclaration {line=0;column=0;contents=("return_" ^ s.contents)}
   |LStar e -> make_tag e
@@ -72,6 +75,12 @@ let rec translate_instruction i =
 and translate_instructions is =
   iter is (
     fun cll_instr imp_cycle ->
+    (* On parcours chaque instrcution du bloc d'instruction d'une procedure
+    et on le traduit en code IMP.
+        - Call et Return ne sont pas terminé, pour l'instant, j'ajoute le saut vers la procédure
+            appelé et l'etiquette de retour une fois l'execution de la procédure terminé.contents
+            Du coup, il manque tous les appels a stack_pointer et les modifs de return_adress
+            et frame_pointer *)
       match cll_instr with
       |Call e -> append (append imp_cycle (IMPTree.Goto e)) (make_tag e)
       |Return -> append imp_cycle IMPTree.Exit
@@ -79,7 +88,9 @@ and translate_instructions is =
   ) empty_cycle
 
 and translate_procedure proc_def imp_instr_cycle =
+(* Ajoute le tag correspondant a la procedure dans le code IMP *)
   let tag_proc = TagDeclaration proc_def.name in
+  (* Traduit le bloc d'instruction de la procédure en code IMP *)
   let imp_proc_block = translate_instructions proc_def.block in
   extend (append imp_instr_cycle tag_proc) imp_proc_block 
 
@@ -89,15 +100,20 @@ and translate_procedure proc_def imp_instr_cycle =
    Vérifie la correction du programme et tente de l'optimiser.
 *)
 let cll_to_imp cll_prog =
+  (* - return_address sert à transmettre à une procédure appelée l’adresse à laquelle elle
+      devra revenir une fois qu’elle aura terminé son exécution
+     - frame_pointer contient l’adresse de la cellule courante de la chaîne d’appels
+     Ces deux données sont ajouté au .data du fichier IMP de sortie *)
   let return_adress = {line = 0;column = 0; contents = ("return_adress", 0)} in
   let frame_pointer = {line = 0;column = 0; contents = ("frame_pointer", 0)} in
   let syntax_tree,data = match cll_prog.syntax_tree with
     |Procedure_Definition_Data (proc_def_cycle,data_cycle) -> 
-      let new_data_cycle = prepend (prepend data_cycle return_adress) frame_pointer in
+      let new_data_cycle = append (prepend data_cycle return_adress) frame_pointer in
+      (* Parcours le cycle de procedure et traduit chaque procedure en code IMP correspondant *)
       let proc_def_to_imp = iter proc_def_cycle translate_procedure empty_cycle in
       (proc_def_to_imp,new_data_cycle)
     |Procedure_Definition proc_def_cycle ->
-      let data_cycle = prepend (prepend empty_cycle return_adress) frame_pointer in
+      let data_cycle = append (prepend empty_cycle return_adress) frame_pointer in
       let proc_def_to_imp = iter proc_def_cycle translate_procedure empty_cycle in
       (proc_def_to_imp, data_cycle)
   in
